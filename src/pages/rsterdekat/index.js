ab7e95f5ab7ab05c3e7dfdacf6d932b9e1f644e8
ihsanuddienullah <ihsanuddienullah@gmail.com>
2020-12-29 18:55:18 +0700

    rebuild

--- src/pages/rsterdekat/index.js
+++ src/pages/rsterdekat/index.js
@@ -6,9 +6,7 @@ const RsTerdekat = () => {
   return (
     <div className="page-rs">
       <h1>Daftar Rumah Sakit</h1>
-      <p>
-        Temukan rumah sakit rujukan covid terdekat untuk penangan lebih cepat
-      </p>
+      <p>Temukan rumah sakit terdekat untuk penangan lebih cepat</p>
       <TableRs />
     </div>
   );
