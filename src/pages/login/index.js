2d0bae7319cd0a0a7e114eb7b47d049f05733530
ihsanuddienullah <ihsanuddienullah@gmail.com>
2021-01-04 19:17:09 +0700

    fix cookie

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBBtn } from 'mdbreact';
-import cookie from 'react-cookie';
+import { useCookies } from 'react-cookie';
 import { setCookie } from '../../utils/cookie';
 import { authService } from '../../services';
 import './style.css';
@@ -12,7 +12,7 @@ const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isLoginLoading, setLoginLoading] = useState(false);
-  const [tryCookie, setTryCookie] = useState(cookie.load('tryCookie'));
+  const [cookies, setCookies, removeCookies] = useCookies([]);
   const onSubmitLogin = () => {
     setLoginLoading(true);
     authService
@@ -23,7 +23,7 @@ const Login = () => {
         const cookieEmail = res.email;
         setCookie('userData', JSON.stringify(cookieEmail), 10000);
         setCookie('token', JSON.stringify(cookieToken), 10000);
-        cookie.save('tryCookie', true, { path: '/' });
+        setCookies('token');
         window.location.assign('/Beranda');
       })
       .catch((err) => {
